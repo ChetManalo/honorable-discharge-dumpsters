2f775080ccd0a49cc2d62f01470ebe94b36eb6b4
ManaloChet-FS <CDManalo@student.fullsail.edu>
2024-11-19 13:16:58 -0500

    Added contract warning text, favicon

--- app/rent-a-dumpster/book/page.tsx
+++ app/rent-a-dumpster/book/page.tsx
@@ -1,7 +1,8 @@
 export default function page(){
     return (
-        <main className="h-svh">
-            <iframe src="https://app.squareup.com/appointments/buyer/widget/k0ambnjznrhlhv/LQF6S4X2MZGCR" className="w-full h-full" frameBorder="0"></iframe>
+        <main className="bg-white">
+            <h1 className="font-bold text-center md:text-lg p-4 text-balance">After booking a dumpster, make sure to sign the contract that is sent to your email. If the contract is not signed, the booking will be canceled.</h1>
+            <iframe src="https://app.squareup.com/appointments/buyer/widget/k0ambnjznrhlhv/LQF6S4X2MZGCR" className="w-full h-screen"></iframe>
         </main>
     )
 }
